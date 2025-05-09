91c871e9e4423fd5a69199d076c17a5d1fdaf6c9
CarlosSQH <carlos.h.borges7@aluno.senai.br>
2025-05-09 15:02:12 -0300

    Add files via upload

---
+++ js/func.js
@@ -0,0 +1,11 @@
+function botao1() {
+  let click1 = window.alert("Você clicou no Botão 1");
+}
+
+function botao2() {
+  let click1 = window.alert("Você clicou no Botão 2");
+}
+
+function botao3() {
+  let click1 = window.alert("Você clicou no Botão 3");
+}
